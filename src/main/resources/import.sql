649625cfec6069761187d11b323af7c19ea468cf
Matheus <matheus.mnascimento@tivit.com>
2024-07-23 17:07:15 -0300

    bcrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$TezdX8fCJ2o61s6aQSW1zuy11bk3eyXRGdIt3zcEDI7Tanc16Aw62');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$TezdX8fCJ2o61s6aQSW1zuy11bk3eyXRGdIt3zcEDI7Tanc16Aw62');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
